4f29c196326fa7a5b705da5255f535e9c2b953bd
Leevon Levasseur <leevonlevasseur@gmail.com>
2023-05-29 11:12:17 -0700

    Release 0.1 - First publish

---
+++ test-app/forge.config.js
@@ -0,0 +1,35 @@
+require( 'dotenv' ).config();
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    osxSign: {
+      optionsForFile: ( filePath ) => {
+        return {
+          entitlements: 'build/entitlements.mac.inherit.plist'
+        }
+      }
+    },
+    osxNotarize: {
+      tool: 'legacy',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    }
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        certificateFile: 'build/windowsCert_SW.pfx',
+        certificatePassword: process.env.CERTIFICATEPASS,
+        setupIcon: 'build/icons/dogicon.ico' 
+      },
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+      }
+    }
+  ],
+};
